344d83a8c47b5017f8cf5c7adb35a624f3ba0afc
Алексей <threat70@gmail.com>
2015-02-20 22:45:25 +0600

    mock supported

---
+++ calc/Model/CloneableMock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calc.Model
+{
+    public class CloneableMock : ICloneable
+    {
+        private int _cloneCallCount = 0;
+
+        public int GetCloneCallCount()
+        {
+            return _cloneCallCount;
+        }
+
+        public object Clone()
+        {
+            _cloneCallCount++;
+            return new CloneableMock();
+        }
+    }
+}
